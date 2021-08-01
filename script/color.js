b84fd8a47f3b237766f245d0fe6f57187fdb6e88
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2021-08-01 13:29:42 +0900

    Add files via upload

---
+++ script/color.js
@@ -0,0 +1,142 @@
+// black = [
+//     "https://pokeapi.co/api/v2/pokemon/143/",
+//     "https://pokeapi.co/api/v2/pokemon/198/",
+//     "https://pokeapi.co/api/v2/pokemon/201/",
+//     "https://pokeapi.co/api/v2/pokemon/215/",
+//     "https://pokeapi.co/api/v2/pokemon/197/",
+//     "https://pokeapi.co/api/v2/pokemon/228/",
+//     "https://pokeapi.co/api/v2/pokemon/229/",
+//     "https://pokeapi.co/api/v2/pokemon/303/",
+//     "https://pokeapi.co/api/v2/pokemon/325/",
+//     "https://pokeapi.co/api/v2/pokemon/336/",
+//     "https://pokeapi.co/api/v2/pokemon/353/",
+//     "https://pokeapi.co/api/v2/pokemon/355/",
+//     "https://pokeapi.co/api/v2/pokemon/344/",
+//     "https://pokeapi.co/api/v2/pokemon/354/",
+//     "https://pokeapi.co/api/v2/pokemon/356/",
+//     "https://pokeapi.co/api/v2/pokemon/441/",
+//     "https://pokeapi.co/api/v2/pokemon/446/",
+//     "https://pokeapi.co/api/v2/pokemon/430/",
+//     "https://pokeapi.co/api/v2/pokemon/487/",
+//     "https://pokeapi.co/api/v2/pokemon/491/",
+//     "https://pokeapi.co/api/v2/pokemon/522/",
+//     "https://pokeapi.co/api/v2/pokemon/461/",
+//     "https://pokeapi.co/api/v2/pokemon/477/",
+//     "https://pokeapi.co/api/v2/pokemon/523/",
+//     "https://pokeapi.co/api/v2/pokemon/561/",
+//     "https://pokeapi.co/api/v2/pokemon/562/",
+//     "https://pokeapi.co/api/v2/pokemon/608/",
+//     "https://pokeapi.co/api/v2/pokemon/644/",
+//     "https://pokeapi.co/api/v2/pokemon/664/",
+//     "https://pokeapi.co/api/v2/pokemon/665/",
+//     "https://pokeapi.co/api/v2/pokemon/731/",
+//     "https://pokeapi.co/api/v2/pokemon/757/",
+//     "https://pokeapi.co/api/v2/pokemon/732/",
+//     "https://pokeapi.co/api/v2/pokemon/733/",
+//     "https://pokeapi.co/api/v2/pokemon/758/",
+//     "https://pokeapi.co/api/v2/pokemon/771/",
+//     "https://pokeapi.co/api/v2/pokemon/796/",
+//     "https://pokeapi.co/api/v2/pokemon/799/",
+//     "https://pokeapi.co/api/v2/pokemon/800/",
+//     "https://pokeapi.co/api/v2/pokemon/837/",
+//     "https://pokeapi.co/api/v2/pokemon/838/",
+//     "https://pokeapi.co/api/v2/pokemon/839/",
+//     "https://pokeapi.co/api/v2/pokemon/897/",
+//     "https://pokeapi.co/api/v2/pokemon/609/",
+// ]
+
+// black = ['snorlax', 'murkrow', 'unown', 'sneasel', 'umbreon', 'houndour', 'houndoom', 'mawile', 'spoink', 'seviper', 'shuppet', 'duskull', 'claydol', 'banette', 'dusclops', 'chatot', 'munchlax', 'honchkrow', 'giratina', 'darkrai', 'blitzle', 'weavile', 'dusknoir', 'zebstrika', 'sigilyph', 'yamask', 'lampent', 'zekrom', 'scatterbug', 'spewpa', 'pikipek', 'salandit', 'trumbeak', 'toucannon', 'salazzle', 'pyukumuku', 'xurkitree', 'guzzlord', 'necrozma', 'rolycoly', 'carkol', 'coalossal', 'spectrier', 'chandelure']
+
+// blue = ['squirtle', 'nidoran-f', 'oddish', 'poliwag', 'tentacool', 'wartortle', 'blastoise', 'nidorina', 'nidoqueen', 'gloom', 'golduck', 'poliwhirl', 'poliwrath', 'tentacruel', 'tangela', 'horsea', 'lapras', 'omanyte', 'articuno', 'dratini', 'seadra', 'gyarados', 'vaporeon', 'omastar', 'dragonair', 'totodile', 'chinchou', 'wooper', 'heracross', 'croconaw', 'feraligatr', 'lanturn', 'marill', 'azumarill', 'jumpluff', 'quagsire', 'wobbuffet', 'phanpy', 'suicune', 'mudkip', 'taillow', 'surskit', 'azurill', 'kingdra', 'marshtomp', 'swampert', 'swellow', 'masquerain', 'loudred', 'exploud', 'meditite', 'wailmer', 'swablu', 'wynaut', 'spheal', 'clamperl', 'bagon', 'beldum', 'sharpedo', 'wailord', 'altaria', 'whiscash', 'chimecho', 'sealeo', 'walrein', 'huntail', 'salamence', 'metang', 'regice', 'latios', 'kyogre', 'piplup', 'shinx', 'cranidos', 'gible', 'riolu', 'croagunk', 'prinplup', 'empoleon', 'luxio', 'luxray', 'rampardos', 'gabite', 'garchomp', 'lucario', 'finneon', 'mantyke', 'azelf', 'phione', 'manaphy', 'oshawott', 'panpour', 'roggenrola', 'woobat', 'lumineon', 'tangrowth', 'glaceon', 'dewott', 'samurott', 'simipour', 'boldore', 'gigalith', 'swoobat', 'tympole', 'sawk', 'tirtouga', 'ducklett', 'karrablast', 'elgyem', 'palpitoad', 'seismitoad', 'carracosta', 'eelektrik', 'eelektross', 'cryogonal', 'deino', 'cobalion', 'thundurus', 'froakie', 'inkay', 'zweilous', 'hydreigon', 'frogadier', 'greninja', 'meowstic', 'clauncher', 'amaura', 'bergmite', 'xerneas', 'popplio', 'wishiwashi', 'mareanie', 'clawitzer', 'aurorus', 'avalugg', 'primarina', 'vikavolt', 'toxapex', 'komala', 'cosmog', 'sobble', 'rookidee', 'blipbug', 'cosmoem', 'drizzile', 'inteleon', 'corvisquire', 'cramorant', 'eiscue', 'arctozolt', 'arctovish', 'zacian', 'metagross', 'toxicroak', 'malamar', 'brionne', 'grapploct']
+
+// blown = ['weedle', 'pidgey', 'spearow', 'vulpix', 'diglett', 'mankey', 'growlithe', 'abra', 'geodude', 'pidgeotto', 'pidgeot', 'raticate', 'fearow', 'dugtrio', 'primeape', 'arcanine', 'kadabra', 'alakazam', 'graveler', 'farfetchd', 'doduo', 'cubone', 'kangaskhan', 'staryu', 'pinsir', 'tauros', 'eevee', 'kabuto', 'dodrio', 'marowak', 'hitmonlee', 'hitmonchan', 'kabutops', 'dragonite', 'sentret', 'hoothoot', 'teddiursa', 'swinub', 'furret', 'noctowl', 'sudowoodo', 'ursaring', 'piloswine', 'stantler', 'entei', 'zigzagoon', 'seedot', 'shroomish', 'slakoth', 'hitmontop', 'nuzleaf', 'shiftry', 'slaking', 'shedinja', 'hariyama', 'torkoal', 'spinda', 'trapinch', 'baltoy', 'feebas', 'regirock', 'chimchar', 'starly', 'bidoof', 'buizel', 'buneary', 'bonsly', 'hippopotas', 'infernape', 'staravia', 'staraptor', 'bibarel', 'floatzel', 'lopunny', 'hippowdon', 'heatran', 'patrat', 'lillipup', 'watchog', 'sandile', 'conkeldurr', 'krokorok', 'sawsbuck', 'beheeyem', 'stunfisk', 'bouffalant', 'vullaby', 'landorus', 'bunnelby', 'litleo', 'skiddo', 'honedge', 'mandibuzz', 'diggersby', 'pyroar', 'gogoat', 'doublade', 'aegislash', 'binacle', 'skrelp', 'tyrunt', 'phantump', 'pumpkaboo', 'volcanion', 'rowlet', 'yungoos', 'rockruff', 'mudbray', 'barbaracle', 'dragalge', 'trevenant', 'gourgeist', 'dartrix', 'decidueye', 'gumshoos', 'lycanroc', 'mudsdale', 'sandygast', 'minior', 'skwovet', 'nickit', 'palossand', 'greedent', 'thievul', 'arrokuda', 'clobbopus', 'golem', 'monferno', 'mamoswine', 'barraskewda', 'perrserker']
+
+// gray = ['machop', 'machoke', 'machamp', 'magnemite', 'onix', 'rhyhorn', 'magneton', 'rhydon', 'misdreavus', 'pineco', 'qwilfish', 'remoraid', 'steelix', 'skarmory', 'poochyena', 'nincada', 'nosepass', 'donphan', 'pupitar', 'mightyena', 'aron', 'volbeat', 'barboach', 'anorith', 'castform', 'snorunt', 'relicanth', 'lairon', 'aggron', 'armaldo', 'glalie', 'registeel', 'shieldon', 'glameow', 'bastiodon', 'purugly', 'pidove', 'drilbur', 'magnezone', 'rhyperior', 'probopass', 'stoutland', 'tranquill', 'unfezant', 'excadrill', 'timburr', 'zorua', 'minccino', 'ferroseed', 'klink', 'whirlipede', 'zoroark', 'cinccino', 'escavalier', 'ferrothorn', 'klang', 'durant', 'terrakion', 'kyurem', 'espurr', 'carbink', 'klefki', 'grubbin', 'wimpod', 'type-null', 'togedemaru', 'jangmo-o', 'magearna', 'marshadow', 'stakataka', 'meltan', 'silvally', 'hakamo-o', 'kommo-o', 'melmetal', 'raboot', 'stonjourner', 'kubfu', 'herdier', 'gurdurr', 'klinklang', 'obstagoon', 'runerigus', 'urshifu', 'golisopod']
+
+// green = ['bulbasaur', 'caterpie', 'bellsprout', 'venusaur', 'metapod', 'weepinbell', 'victreebel', 'scyther', 'chikorita', 'spinarak', 'natu', 'meganium', 'xatu', 'bellossom', 'politoed', 'skiploom', 'larvitar', 'celebi', 'treecko', 'lotad', 'tyranitar', 'grovyle', 'sceptile', 'dustox', 'lombre', 'ludicolo', 'breloom', 'electrike', 'gulpin', 'cacnea', 'kecleon', 'tropius', 'roselia', 'vibrava', 'flygon', 'cacturne', 'cradily', 'rayquaza', 'turtwig', 'budew', 'burmy', 'bronzor', 'grotle', 'torterra', 'roserade', 'wormadam', 'bronzong', 'carnivine', 'shaymin', 'snivy', 'pansage', 'yanmega', 'leafeon', 'servine', 'serperior', 'simisage', 'cottonee', 'petilil', 'basculin', 'maractus', 'trubbish', 'solosis', 'swadloon', 'whimsicott', 'lilligant', 'garbodor', 'duosion', 'reuniclus', 'axew', 'golett', 'virizion', 'tornadus', 'chespin', 'fraxure', 'golurk', 'quilladin', 'chesnaught', 'hawlucha', 'zygarde', 'dewpider', 'comfey', 'charjabug', 'araquanid', 'dhelmise', 'celesteela', 'grookey', 'gossifleur', 'chewtle', 'applin', 'thwackey', 'rillaboom', 'eldegoss', 'drednaw', 'flapple', 'silicobra', 'dracozolt', 'dracovish', 'dreepy', 'zarude', 'regidrago', 'calyrex', 'ivysaur', 'bayleef', 'sandaconda', 'copperajah', 'drakloak', 'dragapult', 'appletun']
+
+// pink = ['clefairy', 'clefable', 'jigglypuff', 'wigglytuff', 'slowpoke', 'exeggcute', 'lickitung', 'porygon', 'slowbro', 'chansey', 'mr-mime', 'mew', 'cleffa', 'igglybuff', 'hoppip', 'snubbull', 'corsola', 'flaaffy', 'slowking', 'smoochum', 'miltank', 'whismur', 'skitty', 'blissey', 'luvdisc', 'milotic', 'gorebyss', 'cherubi', 'mime-jr', 'happiny', 'mesprit', 'munna', 'audino', 'lickilicky', 'musharna', 'deerling', 'alomomola', 'spritzee', 'aromatisse', 'diancie', 'fomantis', 'stufful', 'sylveon', 'lurantis', 'bewear', 'bruxish', 'tapu-lele', 'hatenna', 'impidimp', 'hattrem', 'morgrem', 'hatterene']
+
+// perple = ['rattata', 'ekans', 'nidoran-m', 'zubat', 'venonat', 'arbok', 'nidorino', 'nidoking', 'golbat', 'venomoth', 'grimer', 'shellder', 'gastly', 'koffing', 'ditto', 'aerodactyl', 'mewtwo', 'muk', 'cloyster', 'haunter', 'gengar', 'weezing', 'starmie', 'aipom', 'gligar', 'crobat', 'espeon', 'forretress', 'granbull', 'tyrogue', 'cascoon', 'sableye', 'illumise', 'lileep', 'swalot', 'grumpig', 'shellos', 'drifloon', 'stunky', 'spiritomb', 'skorupi', 'cherrim', 'ambipom', 'drifblim', 'mismagius', 'skuntank', 'drapion', 'palkia', 'purrloin', 'gliscor', 'liepard', 'gothita', 'gothorita', 'gothitelle', 'genesect', 'goomy', 'noibat', 'hoopa', 'crabrawler', 'morelull', 'bounsweet', 'sliggoo', 'goodra', 'noivern', 'shiinotic', 'steenee', 'tapu-fini', 'poipole', 'lunala', 'naganadel', 'toxel', 'sinistea', 'pincurchin', 'indeedee', 'eternatus', 'mantine', 'delcatty', 'gastrodon', 'mienshao', 'toxtricity', 'polteageist', 'grimmsnarl', 'mr-rime', 'tsareena', 'corviknight']
+
+// red = ['charmander', 'paras', 'charmeleon', 'charizard', 'vileplume', 'parasect', 'krabby', 'voltorb', 'goldeen', 'magikarp', 'kingler', 'electrode', 'seaking', 'jynx', 'magmar', 'flareon', 'ledyba', 'yanma', 'slugma', 'delibird', 'ledian', 'ariados', 'scizor', 'magcargo', 'octillery', 'magby', 'ho-oh', 'torchic', 'wurmple', 'porygon2', 'combusken', 'blaziken', 'carvanha', 'solrock', 'corphish', 'medicham', 'camerupt', 'crawdaunt', 'latias', 'groudon', 'deoxys', 'kricketot', 'kricketune', 'rotom', 'tepig', 'pansear', 'magmortar', 'porygon-z', 'pignite', 'emboar', 'simisear', 'throh', 'venipede', 'darumaka', 'dwebble', 'scolipede', 'krookodile', 'darmanitan', 'crustle', 'scrafty', 'shelmet', 'druddigon', 'pawniard', 'heatmor', 'fennekin', 'fletchling', 'accelgor', 'bisharp', 'braviary', 'braixen', 'delphox', 'fletchinder', 'talonflame', 'yveltal', 'litten', 'oricorio', 'torracat', 'incineroar', 'turtonator', 'tapu-bulu', 'buzzwole', 'orbeetle', 'sizzlipede', 'zamazenta', 'tyrantrum', 'centiskorch']
+
+// white = ['butterfree', 'seel', 'dewgong', 'togepi', 'mareep', 'togetic', 'smeargle', 'lugia', 'wingull', 'ralts', 'linoone', 'silcoon', 'kirlia', 'gardevoir', 'vigoroth', 'zangoose', 'absol', 'shelgon', 'pachirisu', 'snover', 'dialga', 'regigigas', 'arceus', 'abomasnow', 'togekiss', 'gallade', 'froslass', 'vanillite', 'emolga', 'foongus', 'frillish', 'tynamo', 'litwick', 'swanna', 'vanillish', 'vanilluxe', 'amoonguss', 'jellicent', 'cubchoo', 'rufflet', 'larvesta', 'reshiram', 'meloetta', 'flabebe', 'pancham', 'furfrou', 'swirlix', 'beartic', 'volcarona', 'floette', 'florges', 'pangoro', 'slurpuff', 'crabominable', 'oranguru', 'passimian', 'drampa', 'nihilego', 'pheromosa', 'kartana', 'blacephalon', 'scorbunny', 'wooloo', 'solgaleo', 'cinderace', 'dubwool', 'milcery', 'snom', 'duraludon', 'glastrier', 'vivillon', 'cursola', 'sirfetchd', 'alcremie', 'frosmoth']
+
+// yellow = ['sandshrew', 'meowth', 'psyduck', 'kakuna', 'beedrill', 'pikachu', 'raichu', 'sandslash', 'ninetales', 'persian', 'ponyta', 'drowzee', 'zapdos', 'moltres', 'rapidash', 'hypno', 'exeggutor', 'electabuzz', 'jolteon', 'cyndaquil', 'pichu', 'sunkern', 'girafarig', 'dunsparce', 'shuckle', 'quilava', 'typhlosion', 'ampharos', 'sunflora', 'elekid', 'raikou', 'makuhita', 'beautifly', 'pelipper', 'ninjask', 'plusle', 'minun', 'numel', 'lunatone', 'manectric', 'jirachi', 'combee', 'chingling', 'mothim', 'vespiquen', 'uxie', 'cresselia', 'victini', 'electivire', 'sewaddle', 'scraggy', 'archen', 'joltik', 'leavanny', 'archeops', 'galvantula', 'mienfoo', 'keldeo', 'haxorus', 'helioptile', 'dedenne', 'cutiefly', 'heliolisk', 'ribombee', 'mimikyu', 'tapu-koko', 'zeraora', 'yamper', 'dottler', 'boltund', 'falinks', 'morpeko', 'cufant', 'regieleki', 'cofagrigus']
+
+
+// 入力フォームの値を取得する
+function getNum() {
+    let num = document.getElementById('color-No').value;
+    return num;
+}
+
+
+// 色別のポケモン名を取得し、配列に保存
+async function getNameData() {
+    colorUrl = `https://pokeapi.co/api/v2/pokemon-color/${getNum()}/`
+
+    res = await fetch(colorUrl);
+    data = await res.json();
+
+    let data_length = data.pokemon_species.length;
+    let names = [];
+
+    for (i = 0; i < data_length; i++) {
+        names.push(data['pokemon_species'][i]['name']);
+    }
+
+    return names;
+}
+
+// 当該の色のポケモン画像を表示する
+// async function displayImage() {
+//     names = await getNameData();
+//     let names_length = names.length;
+
+//     // データ数を表示する
+//     document.getElementById('count').textContent = `${names_length}匹`;
+
+//     for (i = 0; i < names_length; i++) {
+//         let pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${names[i]}/`;
+
+//         res = await fetch(pokemonUrl);
+//         data = await res.json();
+
+
+//         let pokeImageUrl = data['sprites']['front_default']
+
+
+//         let img = document.createElement('img');
+//         img.src = pokeImageUrl;
+
+//         document.getElementById('result').appendChild(img);
+
+//     }
+// }
+
+async function displayImage() {
+    names = await getNameData();
+    let names_length = names.length;
+
+    let pokemonUrl = [];
+    let imageUrl = [];
+
+    let img = [];
+
+    for (i = 0; i < names_length; i++) {
+        pokemonUrl.push(`https://pokeapi.co/api/v2/pokemon/${names[i]}/`);
+    }
+
+    for (i = 0; i < names_length; i++) {
+        res = await fetch(pokemonUrl[i]);
+        data = await res.json();
+        imageUrl = data['sprites']['front_default'];
+        img = document.createElement('img');
+        img.src = imageUrl;
+        document.getElementById('result').appendChild(img);
+
+    }
+
+}
---
+++ script/first-data.js
@@ -0,0 +1,677 @@
+data = [
+    {
+        "ja": "フシギダネ",
+        "en": "Bulbasaur"
+    },
+    {
+        "ja": "フシギソウ",
+        "en": "Ivysaur"
+    },
+    {
+        "ja": "フシギバナ",
+        "en": "Venusaur"
+    },
+    {
+        "ja": "ヒトカゲ",
+        "en": "Charmander"
+    },
+    {
+        "ja": "リザード",
+        "en": "Charmeleon"
+    },
+    {
+        "ja": "リザードン",
+        "en": "Charizard"
+    },
+    {
+        "ja": "ゼニガメ",
+        "en": "Squirtle"
+    },
+    {
+        "ja": "カメール",
+        "en": "Wartortle"
+    },
+    {
+        "ja": "カメックス",
+        "en": "Blastoise"
+    },
+    {
+        "ja": "キャタピー",
+        "en": "Caterpie"
+    },
+    {
+        "ja": "トランセル",
+        "en": "Metapod"
+    },
+    {
+        "ja": "バタフリー",
+        "en": "Butterfree"
+    },
+    {
+        "ja": "ビードル",
+        "en": "Weedle"
+    },
+    {
+        "ja": "コクーン",
+        "en": "Kakuna"
+    },
+    {
+        "ja": "スピアー",
+        "en": "Beedrill"
+    },
+    {
+        "ja": "ポッポ",
+        "en": "Pidgey"
+    },
+    {
+        "ja": "ピジョン",
+        "en": "Pidgeotto"
+    },
+    {
+        "ja": "ピジョット",
+        "en": "Pidgeot"
+    },
+    {
+        "ja": "コラッタ",
+        "en": "Rattata"
+    },
+    {
+        "ja": "ラッタ",
+        "en": "Raticate"
+    },
+    {
+        "ja": "オニスズメ",
+        "en": "Spearow"
+    },
+    {
+        "ja": "オニドリル",
+        "en": "Fearow"
+    },
+    {
+        "ja": "アーボ",
+        "en": "Ekans"
+    },
+    {
+        "ja": "アーボック",
+        "en": "Arbok"
+    },
+    {
+        "ja": "ピカチュウ",
+        "en": "Pikachu"
+    },
+    {
+        "ja": "ライチュウ",
+        "en": "Raichu"
+    },
+    {
+        "ja": "サンド",
+        "en": "Sandshrew"
+    },
+    {
+        "ja": "サンドパン",
+        "en": "Sandslash"
+    },
+    {
+        "ja": "ニドラン♀",
+        "en": "Nidoran♀"
+    },
+    {
+        "ja": "ニドリーナ",
+        "en": "Nidorina"
+    },
+    {
+        "ja": "ニドクイン",
+        "en": "Nidoqueen"
+    },
+    {
+        "ja": "ニドラン♂",
+        "en": "Nidoran♂"
+    },
+    {
+        "ja": "ニドリーノ",
+        "en": "Nidorino"
+    },
+    {
+        "ja": "ニドキング",
+        "en": "Nidoking"
+    },
+    {
+        "ja": "ピッピ",
+        "en": "Clefairy"
+    },
+    {
+        "ja": "ピクシー",
+        "en": "Clefable"
+    },
+    {
+        "ja": "ロコン",
+        "en": "Vulpix"
+    },
+    {
+        "ja": "キュウコン",
+        "en": "Ninetales"
+    },
+    {
+        "ja": "プリン",
+        "en": "Jigglypuff"
+    },
+    {
+        "ja": "プクリン",
+        "en": "Wigglytuff"
+    },
+    {
+        "ja": "ズバット",
+        "en": "Zubat"
+    },
+    {
+        "ja": "ゴルバット",
+        "en": "Golbat"
+    },
+    {
+        "ja": "ナゾノクサ",
+        "en": "Oddish"
+    },
+    {
+        "ja": "クサイハナ",
+        "en": "Gloom"
+    },
+    {
+        "ja": "ラフレシア",
+        "en": "Vileplume"
+    },
+    {
+        "ja": "パラス",
+        "en": "Paras"
+    },
+    {
+        "ja": "パラセクト",
+        "en": "Parasect"
+    },
+    {
+        "ja": "コンパン",
+        "en": "Venonat"
+    },
+    {
+        "ja": "モルフォン",
+        "en": "Venomoth"
+    },
+    {
+        "ja": "ディグダ",
+        "en": "Diglett"
+    },
+    {
+        "ja": "ダグトリオ",
+        "en": "Dugtrio"
+    },
+    {
+        "ja": "ニャース",
+        "en": "Meowth"
+    },
+    {
+        "ja": "ペルシアン",
+        "en": "Persian"
+    },
+    {
+        "ja": "コダック",
+        "en": "Psyduck"
+    },
+    {
+        "ja": "ゴルダック",
+        "en": "Golduck"
+    },
+    {
+        "ja": "マンキー",
+        "en": "Mankey"
+    },
+    {
+        "ja": "オコリザル",
+        "en": "Primeape"
+    },
+    {
+        "ja": "ガーディ",
+        "en": "Growlithe"
+    },
+    {
+        "ja": "ウインディ",
+        "en": "Arcanine"
+    },
+    {
+        "ja": "ニョロモ",
+        "en": "Poliwag"
+    },
+    {
+        "ja": "ニョロゾ",
+        "en": "Poliwhirl"
+    },
+    {
+        "ja": "ニョロボン",
+        "en": "Poliwrath"
+    },
+    {
+        "ja": "ケーシィ",
+        "en": "Abra"
+    },
+    {
+        "ja": "ユンゲラー",
+        "en": "Kadabra"
+    },
+    {
+        "ja": "フーディン",
+        "en": "Alakazam"
+    },
+    {
+        "ja": "ワンリキー",
+        "en": "Machop"
+    },
+    {
+        "ja": "ゴーリキー",
+        "en": "Machoke"
+    },
+    {
+        "ja": "カイリキー",
+        "en": "Machamp"
+    },
+    {
+        "ja": "マダツボミ",
+        "en": "Bellsprout"
+    },
+    {
+        "ja": "ウツドン",
+        "en": "Weepinbell"
+    },
+    {
+        "ja": "ウツボット",
+        "en": "Victreebel"
+    },
+    {
+        "ja": "メノクラゲ",
+        "en": "Tentacool"
+    },
+    {
+        "ja": "ドククラゲ",
+        "en": "Tentacruel"
+    },
+    {
+        "ja": "イシツブテ",
+        "en": "Geodude"
+    },
+    {
+        "ja": "ゴローン",
+        "en": "Graveler"
+    },
+    {
+        "ja": "ゴローニャ",
+        "en": "Golem"
+    },
+    {
+        "ja": "ポニータ",
+        "en": "Ponyta"
+    },
+    {
+        "ja": "ギャロップ",
+        "en": "Rapidash"
+    },
+    {
+        "ja": "ヤドン",
+        "en": "Slowpoke"
+    },
+    {
+        "ja": "ヤドラン",
+        "en": "Slowbro"
+    },
+    {
+        "ja": "コイル",
+        "en": "Magnemite"
+    },
+    {
+        "ja": "レアコイル",
+        "en": "Magneton"
+    },
+    {
+        "ja": "カモネギ",
+        "en": "Farfetch'd"
+    },
+    {
+        "ja": "ドードー",
+        "en": "Doduo"
+    },
+    {
+        "ja": "ドードリオ",
+        "en": "Dodrio"
+    },
+    {
+        "ja": "パウワウ",
+        "en": "Seel"
+    },
+    {
+        "ja": "ジュゴン",
+        "en": "Dewgong"
+    },
+    {
+        "ja": "ベトベター",
+        "en": "Grimer"
+    },
+    {
+        "ja": "ベトベトン",
+        "en": "Muk"
+    },
+    {
+        "ja": "シェルダー",
+        "en": "Shellder"
+    },
+    {
+        "ja": "パルシェン",
+        "en": "Cloyster"
+    },
+    {
+        "ja": "ゴース",
+        "en": "Gastly"
+    },
+    {
+        "ja": "ゴースト",
+        "en": "Haunter"
+    },
+    {
+        "ja": "ゲンガー",
+        "en": "Gengar"
+    },
+    {
+        "ja": "イワーク",
+        "en": "Onix"
+    },
+    {
+        "ja": "スリープ",
+        "en": "Drowzee"
+    },
+    {
+        "ja": "スリーパー",
+        "en": "Hypno"
+    },
+    {
+        "ja": "クラブ",
+        "en": "Krabby"
+    },
+    {
+        "ja": "キングラー",
+        "en": "Kingler"
+    },
+    {
+        "ja": "ビリリダマ",
+        "en": "Voltorb"
+    },
+    {
+        "ja": "マルマイン",
+        "en": "Electrode"
+    },
+    {
+        "ja": "タマタマ",
+        "en": "Exeggcute"
+    },
+    {
+        "ja": "ナッシー",
+        "en": "Exeggutor"
+    },
+    {
+        "ja": "カラカラ",
+        "en": "Cubone"
+    },
+    {
+        "ja": "ガラガラ",
+        "en": "Marowak"
+    },
+    {
+        "ja": "サワムラー",
+        "en": "Hitmonlee"
+    },
+    {
+        "ja": "エビワラー",
+        "en": "Hitmonchan"
+    },
+    {
+        "ja": "ベロリンガ",
+        "en": "Lickitung"
+    },
+    {
+        "ja": "ドガース",
+        "en": "Koffing"
+    },
+    {
+        "ja": "マタドガス",
+        "en": "Weezing"
+    },
+    {
+        "ja": "サイホーン",
+        "en": "Rhyhorn"
+    },
+    {
+        "ja": "サイドン",
+        "en": "Rhydon"
+    },
+    {
+        "ja": "ラッキー",
+        "en": "Chansey"
+    },
+    {
+        "ja": "モンジャラ",
+        "en": "Tangela"
+    },
+    {
+        "ja": "ガルーラ",
+        "en": "Kangaskhan"
+    },
+    {
+        "ja": "タッツー",
+        "en": "Horsea"
+    },
+    {
+        "ja": "シードラ",
+        "en": "Seadra"
+    },
+    {
+        "ja": "トサキント",
+        "en": "Goldeen"
+    },
+    {
+        "ja": "アズマオウ",
+        "en": "Seaking"
+    },
+    {
+        "ja": "ヒトデマン",
+        "en": "Staryu"
+    },
+    {
+        "ja": "スターミー",
+        "en": "Starmie"
+    },
+    {
+        "ja": "バリヤード",
+        "en": "Mr. Mime"
+    },
+    {
+        "ja": "ストライク",
+        "en": "Scyther"
+    },
+    {
+        "ja": "ルージュラ",
+        "en": "Jynx"
+    },
+    {
+        "ja": "エレブー",
+        "en": "Electabuzz"
+    },
+    {
+        "ja": "ブーバー",
+        "en": "Magmar"
+    },
+    {
+        "ja": "カイロス",
+        "en": "Pinsir"
+    },
+    {
+        "ja": "ケンタロス",
+        "en": "Tauros"
+    },
+    {
+        "ja": "コイキング",
+        "en": "Magikarp"
+    },
+    {
+        "ja": "ギャラドス",
+        "en": "Gyarados"
+    },
+    {
+        "ja": "ラプラス",
+        "en": "Lapras"
+    },
+    {
+        "ja": "メタモン",
+        "en": "Ditto"
+    },
+    {
+        "ja": "イーブイ",
+        "en": "Eevee"
+    },
+    {
+        "ja": "シャワーズ",
+        "en": "Vaporeon"
+    },
+    {
+        "ja": "サンダース",
+        "en": "Jolteon"
+    },
+    {
+        "ja": "ブースター",
+        "en": "Flareon"
+    },
+    {
+        "ja": "ポリゴン",
+        "en": "Porygon"
+    },
+    {
+        "ja": "オムナイト",
+        "en": "Omanyte"
+    },
+    {
+        "ja": "オムスター",
+        "en": "Omastar"
+    },
+    {
+        "ja": "カブト",
+        "en": "Kabuto"
+    },
+    {
+        "ja": "カブトプス",
+        "en": "Kabutops"
+    },
+    {
+        "ja": "プテラ",
+        "en": "Aerodactyl"
+    },
+    {
+        "ja": "カビゴン",
+        "en": "Snorlax"
+    },
+    {
+        "ja": "フリーザー",
+        "en": "Articuno"
+    },
+    {
+        "ja": "サンダー",
+        "en": "Zapdos"
+    },
+    {
+        "ja": "ファイヤー",
+        "en": "Moltres"
+    },
+    {
+        "ja": "ミニリュウ",
+        "en": "Dratini"
+    },
+    {
+        "ja": "ハクリュー",
+        "en": "Dragonair"
+    },
+    {
+        "ja": "カイリュー",
+        "en": "Dragonite"
+    },
+    {
+        "ja": "ミュウツー",
+        "en": "Mewtwo"
+    },
+    {
+        "ja": "ミュウ",
+        "en": "Mew"
+    }
+]
+
+// 変換ボタンを押すと日本語名を英語名に変える機能
+function change() {
+    let jaName = document.getElementById('ja-Name').value;
+
+    let matchData = data.filter(function (item, index) {
+        if (item.ja == jaName) return true;
+    });
+
+    return matchData[0].en.toLowerCase();
+}
+
+// 入力フォームの初期化
+function clearText() {
+    let textForm = document.getElementById("ja-Name");
+    textForm.value = '';
+}
+
+async function display() {
+    let enName = change();
+
+    // APIでjsonを取得する
+    let res = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + enName);
+    let data = await res.json();
+
+    // ID、属名を取得する。
+    let pokeId = data['id'];
+    let pokeEnName = enName;
+    let pokeGenus = data['genera'][0]['genus'];
+    var pokemonUrl = data['varieties'][0]['pokemon']['url']
+
+    // 画像データを取得するためエンドポイントを変える。
+    res = await fetch(pokemonUrl)
+    data = await res.json();
+
+    // 画像を取得
+    let pokeImageUrl = data['sprites']['front_default']
+
+
+    // HTMLを生成していく
+    // 1データのdivを生成
+    let tr = document.createElement('tr');
+
+    // 図鑑番号
+    let id = document.createElement('td');
+    id.textContent = pokeId;
+    tr.appendChild(id);
+
+    let janame = document.createElement('td');
+    janame.textContent = document.getElementById('ja-Name').value;
+    tr.appendChild(janame);
+
+    let enname = document.createElement('td');
+    enname.textContent = pokeEnName;
+    tr.appendChild(enname);
+
+    // 属名を挿入
+    let genus = document.createElement('td');
+    genus.textContent = pokeGenus;
+    tr.appendChild(genus);
+
+    // 画像を挿入
+    let img = document.createElement('img');
+    img.src = pokeImageUrl; // 画像パスを追加
+    tr.appendChild(img);
+
+    // 生成したtr要素を、data-table1に追加、表示する
+    document.getElementById('data-table1').appendChild(tr);
+
+    clearText();
+}
